c264b71c31220d5136831fecd96cfe7eaf9d9f0f
Savinov <savinov_vu@mail.ru>
2024-02-13 13:42:34 +0800

    initial commit

---
+++ src/main/java/integration/cb/CbIntegrationApplication.java
@@ -0,0 +1,13 @@
+package integration.cb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbIntegrationApplication.class, args);
+	}
+
+}
